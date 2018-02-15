a59207c432f36d229a436906bbf9dbeff57f0f79
Alexander <dev@salex.pro>
2018-02-16 00:39:57 +0200

    set boilerplate

---
+++ source/js/app.js
@@ -0,0 +1,8 @@
+/* 
+@codekit-prepend '../../node_modules/jquery/dist/jquery.min.js';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
+@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
+*/
+
+$(document).foundation();
