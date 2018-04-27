3292321bec1e1ca2473257fad8d9ddb98f88f565
Alexander <dev@salex.pro>
2018-04-27 19:17:34 +0300

    totaly new design with many fixes

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,31 @@
 /* 
-@codekit-prepend '../../node_modules/jquery/dist/jquery.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
-@codekit-prepend '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
-@codekit-prepend '../../node_modules/owl.carousel/dist/owl.carousel.min.js';
+@codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.timer.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.imageLoader.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.touch.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.smoothScroll.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.magellan.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.orbit.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.sticky.min';
+@codekit-prepend quiet '../../node_modules/owl.carousel/dist/owl.carousel.min';
 */
 
 $(document).foundation();
+
+const nav = $('.nav');
+$(document).scroll(() => {
+    if (nav.offset().top > 50){
+        nav.addClass('is_fixed');
+    } else {
+        nav.removeClass('is_fixed');
+    }
+});
 
 $('.owl-carousel').owlCarousel({
     items: 1,
